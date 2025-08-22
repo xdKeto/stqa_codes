bd90a35f98049db353764b969005560be5a5f097
Alvin Tjondrowiguno <alvin.tjondrowiguno@gmail.com>
2025-08-22 12:39:13 +0700

    Module 03 done

---
+++ 03_test_fixtures/tests/test_account.py
@@ -0,0 +1,27 @@
+"""
+Test Cases TestAccountModel
+"""
+import json
+from unittest import TestCase
+from models import db
+from models.account import Account
+
+ACCOUNT_DATA = {}
+
+class TestAccountModel(TestCase):
+    """Test Account Model"""
+
+    @classmethod
+    def setUpClass(cls):
+        """ Connect and load data needed by tests """
+
+    @classmethod
+    def tearDownClass(cls):
+        """Disconnect from database"""
+
+    def setUp(self):
+        """Truncate the tables"""
+
+    ######################################################################
+    #  T E S T   C A S E S
+    ######################################################################
